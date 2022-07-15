93519b7f9edb46f21d3d56fa87006a9780c87ee5
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2022-07-15 20:21:25 -0300

    desafio

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <h3 class="titulo1">Cerveza Artesanal</h3>
+    <br />
+    <Products   v-for="(product, i) in products"
+      :key="i"
+      :id="product.id"
+      :titulo="product.titulo"
+      :precio="product.precio"
+      :sabor="product.sabor"
+      :imagen="product.imagen"
+      />
+  </div>
+</template>
+
+<script>
+import Products from './components/Products.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Products
+  },
+    data() {
+    return {
+      products: [
+        {
+          id: 1,
+          titulo: "Honey",
+          sabor: "Dulce",
+          precio: 350,
+          imagen:
+            "https://res.cloudinary.com/dytpump6i/image/upload/v1653932402/honey_knvkxz.jpg",
+        },
+        {
+          id: 2,
+          titulo: "Blonde",
+          sabor: "Amarga",
+          precio: 400,
+          imagen:
+            "https://res.cloudinary.com/dytpump6i/image/upload/v1653932534/blonde2_ts180x.jpg",
+        },
+        {
+          id: 3,
+          titulo: "Ipa",
+          sabor: "Amarga",
+          precio: 400,
+          imagen:
+            "https://res.cloudinary.com/dytpump6i/image/upload/v1653932553/ipa_yf51hn.jpg",
+        },
+        {
+          id: 4,
+          titulo: "Scottish",
+          sabor: "Dulce",
+          precio: 350,
+          imagen:
+            "https://res.cloudinary.com/dytpump6i/image/upload/v1653932564/scottish_izjelm.jpg",
+        },
+      ],
+    productosEnCarrito: [],
+    };
+  },
+}
+</script>
+
+<style>
+
+.titulo1 {
+  text-align: center;
+  color: brown;
+}
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="productos">
+      <div class="card" style="width: 18rem">
+        <h5 class="card-title text-center">{{ titulo }}</h5>
+        <img class="img-card" :src="imagen" alt="" />
+        <!-- <img :src="getImgUrl()" class="card-img-top d-block w-50" :alt="titulo"> -->
+        <div class="card-body">
+          <p class="card-text">${{ precio }}</p>
+          <button class="btn btn-warning text-center">Agregar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    titulo: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+    sabor: {
+      type: String,
+      required: true,
+    },
+    imagen: {
+      type: String,
+      required: true,
+    },
+    products: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+
+.productos {
+  text-align: center;
+  padding: 15px;
+  margin: auto;
+  border-radius: 10px;
+  box-shadow: 1px 4px 10px 0 brown;
+  float: right;
+}
+.img-card {
+  width: 150px;
+  height: 150px;
+}
+</style>
